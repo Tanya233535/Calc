15ac5cb478a4fdffc543bd720b07d15168ce87eb
Tanya233535 <233535@edu.fa.ru>
2025-03-30 15:11:27 +0300

    Простая запись в файл

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,38 +2,45 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Calculator {
-    public static void main(String args[]) throws FileNotFoundException{
-        File file = new File("test.txt");
-        Scanner sc = new Scanner(file);
-        String[] s = sc.nextLine().split(" ");
-        double a = 0, b = 0;
-        boolean flag = true;
-        try {
-            a = Double.parseDouble(s[0]);
-            b = Double.parseDouble(s[2]);
-        } catch (NumberFormatException ex){
-            System.out.println("Error! Not number");
-            flag = false;
-            return;
-        }
-        if ("+-*/".contains(s[1]) && flag){
-            switch (s[1]){
-                case "+": {System.out.println(a+b); break;}
-                case "-": {System.out.println(a-b); break;}
-                case "*": {System.out.println(a*b); break;}
-                case "/": {
-                    try {
-                        if(b == 0.0)
-                            System.out.println("Error! Division by zero");
-                        else
-                            System.out.println(a / b);
-                    } catch (ArithmeticException ex) {
-                        System.out.println("Error! Division by zero");
-                    }
-                    break;
-                }
+    public static void main(String args[]) {
+        File inputFile = new File("input.txt");
+        File outputFile  = new File("output.txt");
+
+        try (Scanner sc = new Scanner(inputFile);
+             PrintWriter pw = new PrintWriter(outputFile)) {
+            String[] s = sc.nextLine().split(" ");
+            double a = 0, b = 0;
+            boolean flag = true;
+            String result = "";
+
+            try {
+                a = Double.parseDouble(s[0]);
+                b = Double.parseDouble(s[2]);
+            } catch (NumberFormatException ex){
+                result = "Error! Not number";
+                flag = false;
             }
-        } else
-            System.out.println("Operation Error!");
+
+            if ("+-*/".contains(s[1]) && flag){
+                switch (s[1]){
+                    case "+": {result = String.valueOf(a+b); break;}
+                    case "-": {result = String.valueOf(a-b); break;}
+                    case "*": {result = String.valueOf(a*b); break;}
+                    case "/":
+                        if(b == 0.0) {
+                            result = "Error! Division by zero";
+                        }else{
+                                result = String.valueOf(a / b);
+                        }
+                        break;
+                }
+            } else if (flag) {
+                result = "Operation Error!";
+            }
+            System.out.println(result);
+            pw.println(result);
+             } catch (FileNotFoundException e) {
+            System.out.println("Input file not found.");
+        }
     }
 }
