c9ff076d00c5e5d9fc2ff354592b0ab67f783d0b
Tanya233535 <233535@edu.fa.ru>
2025-03-30 14:31:17 +0300

    Простой калькулятор

---
+++ src/Calculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.nextLine().split(" ");
+        double a = 0, b = 0;
+        boolean flag = true;
+        try {
+            a = Double.parseDouble(s[0]);
+            b = Double.parseDouble(s[2]);
+        } catch (NumberFormatException ex){
+            System.out.println("Error! Not number");
+            flag = false;
+            return;
+        }
+        if ("+-*/".contains(s[1]) && flag){
+            switch (s[1]){
+                case "+": {System.out.println(a+b); break;}
+                case "-": {System.out.println(a-b); break;}
+                case "*": {System.out.println(a*b); break;}
+                case "/": {
+                    try {
+                        if(b == 0.0)
+                            System.out.println("Error! Division by zero");
+                        else
+                            System.out.println(a / b);
+                    } catch (ArithmeticException ex) {
+                        System.out.println("Error! Division by zero");
+                    }
+                    break;
+                }
+            }
+        } else
+            System.out.println("Operation Error!");
+    }
+}
